e181c6e302d0af941e3f1c4e4589056efe826df1
AlecNesatColak <87741998+AlecNesatColak@users.noreply.github.com>
2023-05-10 14:06:43 -0400

    Update Pakuri.java

--- PakudexProject/Pakuri.java
+++ PakudexProject/Pakuri.java
@@ -4,14 +4,18 @@ public class Pakuri {
     
     private String species = null;
     private int attack;
+    private int level;
     private int defense;
     private int speed;
+    private int hp;
 
     public Pakuri(String species) {
         this.species = species;
         this.attack = (species.length() * 7) + 9;
         this.defense = (species.length() * 5) + 17;
         this.speed = (species.length() * 6) + 13;
+        this.level = 1;
+        this.hp = 100;
     }
 
     public String getSpecies() {
@@ -30,14 +34,28 @@ public class Pakuri {
         return speed;
     }
 
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel() {
+        this.level+= 1;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
     public void setAttack(int newAttack) {
         this.attack = newAttack;
     }
 
     public void evolve() {
-        this.attack = 2*attack;
-        this.defense = 4*defense;
-        this.speed = 3*speed;
+        if (level >= 25) {
+            this.attack = 2*attack;
+            this.defense = 4*defense;
+            this.speed = 3*speed;
+        }
     }
 
 
